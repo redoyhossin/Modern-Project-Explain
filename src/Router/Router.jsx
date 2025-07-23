24009af72410b9b357adaed99f6684c065b23969
redoyhossin <redoyhossin5984@gmail.com>
2025-07-24 01:05:30 +0600

    testimonialsPageupdate

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -18,7 +18,7 @@ export const router = createBrowserRouter([
         element:<Card/>
       },
       {
-        path:"testimonials",
+        path:"/testimonials",
         element:<Testimonial/>
       }
     ]
